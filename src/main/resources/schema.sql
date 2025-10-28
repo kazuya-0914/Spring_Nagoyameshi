3e4feff83bb0d037444ca8f215377908e364ed95
kazuya-0914 <kazu.work.0914@gmail.com>
2025-10-29 06:01:56 +0900

    初回コミット

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,23 @@
+CREATE TABLE IF NOT EXISTS roles (
+   id SERIAL PRIMARY KEY,
+   name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS users (
+   id SERIAL PRIMARY KEY,
+   name VARCHAR(50) NOT NULL,
+   furigana VARCHAR(50) NOT NULL,
+   postal_code VARCHAR(50) NOT NULL,
+   address VARCHAR(255) NOT NULL,
+   phone_number VARCHAR(50) NOT NULL,
+   birthday DATE,
+   occupation VARCHAR(50),
+   email VARCHAR(255) NOT NULL UNIQUE,
+   password VARCHAR(255) NOT NULL,
+   role_id INT NOT NULL,
+   enabled BOOLEAN NOT NULL,
+   stripe_customer_id VARCHAR(255) UNIQUE,
+   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+   updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+   FOREIGN KEY (role_id) REFERENCES roles (id)
+);
