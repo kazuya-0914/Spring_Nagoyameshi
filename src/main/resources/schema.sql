e2484c30d11901ef713df6f5b871338dad260e6b
kazuya-0914 <kazu.work.0914@gmail.com>
2025-11-12 07:53:59 +0900

    11/12 コミット

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -21,3 +21,82 @@ CREATE TABLE IF NOT EXISTS users (
    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
    FOREIGN KEY (role_id) REFERENCES roles (id)
 );
+
+CREATE TABLE IF NOT EXISTS verification_tokens (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL UNIQUE,
+    token VARCHAR(255) NOT NULL,
+  	created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  	updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+CREATE TABLE IF NOT EXISTS restaurants (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    image VARCHAR(255),
+    description TEXT NOT NULL,
+    lowest_price INT NOT NULL,
+    highest_price INT NOT NULL,
+    postal_code VARCHAR(50) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    opening_time TIME NOT NULL,
+    closing_time TIME NOT NULL,
+    seating_capacity INT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS categories (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS category_restaurant (
+    id SERIAL PRIMARY KEY,
+    restaurant_id INT NOT NULL,
+    category_id INT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    UNIQUE (restaurant_id, category_id),
+    FOREIGN KEY (restaurant_id) REFERENCES restaurants (id),
+    FOREIGN KEY (category_id) REFERENCES categories (id)
+);
+
+CREATE TABLE IF NOT EXISTS regular_holidays (
+    id SERIAL PRIMARY KEY,
+    day VARCHAR(50) NOT NULL,
+    day_index INT
+);
+
+CREATE TABLE IF NOT EXISTS regular_holiday_restaurant (
+    id SERIAL PRIMARY KEY,
+    restaurant_id INT NOT NULL,
+    regular_holiday_id INT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    UNIQUE (restaurant_id, regular_holiday_id),
+    FOREIGN KEY (restaurant_id) REFERENCES restaurants (id),
+    FOREIGN KEY (regular_holiday_id) REFERENCES regular_holidays (id)
+);
+
+CREATE TABLE IF NOT EXISTS companies (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    postal_code VARCHAR(50) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    representative VARCHAR(50) NOT NULL,
+    establishment_date VARCHAR(50) NOT NULL,
+    capital VARCHAR(50) NOT NULL,
+    business VARCHAR(255) NOT NULL,
+    number_of_employees VARCHAR(50) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS terms (
+    id SERIAL PRIMARY KEY,
+    content TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
